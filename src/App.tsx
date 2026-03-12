3ac8e7e1cfe3ed1d94172f42c833f7f85aa032f0
TalitaFerreiraTR <talita.pereira@thomsonreuters.com>
2026-03-12 15:33:03 -0300

    Configura GitHub Pages com deploy automatico via Actions

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ const App = () => {
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename="/onboardingpfolha">
           <Routes>
             <Route path="/" element={<Onboarding />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
